e01702d68bb3c12bf556f57990ee6ae9c1f34824
jisunglim <jisunglim@nkia.co.kr>
2017-12-14 13:24:08 +0900

    Route require와 use 코드 통합

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ var express = require('express');
 var app = express();
 var expressLayouts = require('express-ejs-layouts');
 
-var route_index = require(__dirname + '/route/index');
-
+/* View Engine */
 app.use(express.static('public'));
 app.set('views', __dirname + '/view');
 app.set('view engine', 'ejs');
 
+/* Express layouts*/
 app.set('layout', 'layout');
 app.set('layout extractScripts', true);
 app.use(expressLayouts);
 
-app.use('/', route_index);
+/* Route */
+app.use('/', require(__dirname + '/route/index'));
 
+/* Listen */
 app.listen(3000, function() {
   console.log('Example App Listening on Port 3000');
 });
